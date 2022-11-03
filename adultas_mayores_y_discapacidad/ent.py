35048253373fe839fd869aa9dabff2fb6719e82b
clautc <claudia.tejos@gmail.com>
2022-11-03 19:56:40 -0300

    incorporación de gitignore y avance gráficos de barra

---
+++ adultas_mayores_y_discapacidad/ent.py
@@ -0,0 +1,6 @@
+import sys
+import os
+sys.path.append('/home/clautc/Proyectos_/cuidados/adultas_mayores_y_discapacidad')
+os.chdir('/home/clautc/Proyectos_/cuidados/adultas_mayores_y_discapacidad')
+import xlsxwriter
+from fun_procesamiento import *
--- adultas_mayores_y_discapacidad/tpe_al.py
+++ adultas_mayores_y_discapacidad/tpe_al.py
@@ -1,15 +1,14 @@
-
+import sys
+import os
+sys.path.append('/home/clautc/Proyectos_/cuidados/adultas_mayores_y_discapacidad')
+os.chdir('/home/clautc/Proyectos_/cuidados/adultas_mayores_y_discapacidad')
 import xlsxwriter
-import matplotlib.pyplot as plt
-import pandas as pd
 from fun_procesamiento import *
-
-os.chdir('/adultas_mayores_y_discapacidad')
 
 # 1) Obtención data
 
 archivo = 'tpALC_jdgo'
-name_sheet = 'datos_planos'
+name_sheet = 'enf_no_transmisibles'
 with open('/adultas_mayores_y_discapacidad/keys/keys.txt') as k:
     keys = k.readline()
 data = obtener_data_google_sheet(name_archivo=archivo, name_sheet=name_sheet, keys=keys)
